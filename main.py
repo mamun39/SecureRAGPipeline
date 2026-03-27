c0b0ecdcc708edf92c59cc2190ef1c145bf6a5ab
Mamun <mamunur.iut@gmail.com>
2026-03-27 07:30:38 -0400

    add custom types for RAG

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from dotenv import load_dotenv
 import uuid
 import os
 import datetime
+from data_loader import load_and_chunk_pdf, embed_texts
+from vector_db import QdrantStorage
+from custom_types import RAGQueryResult, RAGSearchResult, RAGUpsertResult, RAGChunkAndSrc
 
 load_dotenv()
 
